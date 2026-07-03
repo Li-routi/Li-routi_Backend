9bb78d5ec2ccb23969faf2e79d67da5a59a48d02
yousung1020 <choiyousung1020@gmail.com>
2026-07-03 19:46:02 +0900

    chore: 첫 커밋

---
+++ src/main/java/com/lirouti/LiroutiApplication.java
@@ -0,0 +1,13 @@
+package com.lirouti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiroutiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiroutiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lirouti/LiroutiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lirouti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiroutiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
