ea00e25ab77116aaf0c9f1a0bca156fb6c588793
yousung1020 <choiyousung1020@gmail.com>
2026-07-18 15:26:13 +0900

    fix: 전역적으로 테스트 실행 시 test 프로파일 활성화 설정

--- src/test/java/com/lirouti/LiroutiApplicationTests.java
+++ src/test/java/com/lirouti/LiroutiApplicationTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@Disabled
+@Disabled("테스트용 서비스 실행 환경 구성 후 활성화 예정")
 class LiroutiApplicationTests {
 
 	@Test
