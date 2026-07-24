3d013e6f4f2cf1aa51cb721d8f685de6c46fdb33
Junhyuk K <kjh27256901@gmail.com>
2026-07-25 02:14:00 +0900

    feat: 금일 그룹 루틴 조회 API 구현 (#27)

--- src/main/java/com/lirouti/domain/group/exception/code/success/GroupSuccessCode.java
+++ src/main/java/com/lirouti/domain/group/exception/code/success/GroupSuccessCode.java
@@ -8,6 +8,11 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum GroupSuccessCode implements BaseSuccessCode {
+    GROUP_ROUTINE_TODAY_FETCH_SUCCESS(
+            HttpStatus.OK,
+            "오늘의 그룹 루틴 조회에 성공했습니다.",
+            "GROUP200_1"
+    ),
     GROUP_ROUTINE_CREATE_SUCCESS(
             HttpStatus.CREATED,
             "그룹 루틴 생성에 성공했습니다.",
