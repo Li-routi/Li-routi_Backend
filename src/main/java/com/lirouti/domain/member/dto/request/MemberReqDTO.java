995140e4d2d7e8532291ea3981693189a5ab9560
ochyeon <136903107+ochyeon@users.noreply.github.com>
2026-07-30 18:51:05 +0900

    fix: 닉네임 설정 조건에 따른 수정

--- src/main/java/com/lirouti/domain/member/dto/request/MemberReqDTO.java
+++ src/main/java/com/lirouti/domain/member/dto/request/MemberReqDTO.java
@@ -15,9 +15,14 @@ public final class MemberReqDTO {
 
     public record UpdateProfile(
             @NotBlank(message = "닉네임은 필수입니다.")
-            @Size(min = 1, max = 20, message = "닉네임은 10자 이하여야 합니다.")
+            @Size(min = 1, max = 10, message = "닉네임은 10자 이하여야 합니다.")
             String nickname
     ) {
         // 기능명세서 내 '프로필 조회와 닉네임 수정' 페이지에 명시된 닉네임 조건 : 앞 뒤 공백 제거 후 1 - 10자
+        public UpdateProfile {
+            if(nickname != null) {
+                nickname = nickname.strip();
+            }
+        }
     }
 }
