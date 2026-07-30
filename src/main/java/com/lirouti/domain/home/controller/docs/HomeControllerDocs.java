17d23ae44739ce101f2ecbcff920c64aa390a249
ochyeon <136903107+ochyeon@users.noreply.github.com>
2026-07-31 01:11:28 +0900

    chore: 문서 내 오타 수정

--- src/main/java/com/lirouti/domain/home/controller/docs/HomeControllerDocs.java
+++ src/main/java/com/lirouti/domain/home/controller/docs/HomeControllerDocs.java
@@ -25,7 +25,7 @@ public interface HomeControllerDocs {
             ),
             @io.swagger.v3.oas.annotations.responses.ApiResponse(
                     responseCode = "401",
-                    description = "유효하지 않거나 만려된 인증 토큰"
+                    description = "유효하지 않거나 만료된 인증 토큰"
             ),
             @io.swagger.v3.oas.annotations.responses.ApiResponse(
                     responseCode = "404",
